de52035f0d4306e960d2d8b92884cafdc23fee01
Huctica <nfn6789@gmail.com>
2021-03-06 21:04:48 -0800

    Replaced Hello world with basic scaffolding for the "I am rich" app

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("I Am Rich"),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(image: NetworkImage('INSERT URL')),
+          ),
         ),
       ),
     );
